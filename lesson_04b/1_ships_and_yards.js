d17c02b2bb881da108060d2f90762787f475c819
Evgeni Rovneiko <erovneiko@mail.ru>
2022-07-05 11:49:40 +0300

    updates

--- lesson_04b/1_ships_and_yards.js
+++ lesson_04b/1_ships_and_yards.js
@@ -10,11 +10,11 @@ const Yard = function (type) {
   
   this.repair = function (ship) { 
     if (this.check(ship)) 
-      console.log(`Корабль "${ship.name}" отремонтирован`) 
+      Object.getPrototypeOf(ship).repair()
   }
   
-  this.paint = function (ship) { 
-    console.log(`Корабль "${ship.name}" покрашен`) 
+  this.paint = function (ship, color) { 
+    Object.getPrototypeOf(ship).paint(color)
   }
   
   this.change = function (ship1, ship2) { 
@@ -41,48 +41,60 @@ const Yard = function (type) {
 const MotorYard = function () {
   Object.setPrototypeOf(this, new Yard("Motor"))
   
-  this.build = function (name, power, material) {
-    return Object.getPrototypeOf(this).build.call(this, new MotorShip(name, power, material))
+  this.build = function (name, power, material, color) {
+    return Object.getPrototypeOf(this).build.call(this, new MotorShip(name, power, material, color))
   }
 }
 
 const SailingYard = function () {
   Object.setPrototypeOf(this, new Yard("Sailing"))
   
-  this.build = function (name, masts, square) {
-    return Object.getPrototypeOf(this).build.call(this, new SailingShip(name, masts, square))
+  this.build = function (name, masts, square, color) {
+    return Object.getPrototypeOf(this).build.call(this, new SailingShip(name, masts, square, color))
   }
 }
 
-const Ship = function (name, type) {
+const Ship = function (name, type, color) {
   this.type = type
   this.name = name
+  this.color = color
+  this.lastRepair = new Date
+
+  this.repair = function () {
+    this.lastRepair = new Date
+    console.log(`Корабль "${this.name}" отремонтирован "${this.lastRepair.toLocaleDateString()}"`) 
+  }
+
+  this.paint = function (color) {
+    this.color = color
+    console.log(`Корабль "${this.name}" перекрашен в "${this.color}" цвет`) 
+  }
 }
 
-const MotorShip = function (name, power, material) {
+const MotorShip = function (name, power, material, color) {
   this.power = power
   this.material = material
-  Object.setPrototypeOf(this, new Ship(name, "Motor"))
+  Object.setPrototypeOf(this, new Ship(name, "Motor", color))
 }
 
-const SailingShip = function (name, masts, square) {
+const SailingShip = function (name, masts, square, color) {
   this.masts = masts
   this.square = square
-  Object.setPrototypeOf(this, new Ship(name, "Sailing"))  
+  Object.setPrototypeOf(this, new Ship(name, "Sailing", color))  
 }
 
 var motorYard = new MotorYard()
 var sailingYard = new SailingYard()
 
-var motorShip1 = motorYard.build("motorShip1", "100kW", "Wood")   //Корабль "motorShip1" построен
-var motorShip2 = motorYard.build("motorShip2", "200kW", "Metal")  //Корабль "motorShip2" построен
-var sailingShip1 = sailingYard.build("sailingShip1", 3, 100)      //Корабль "sailingShip1" построен
+var motorShip1 = motorYard.build("motorShip1", "100kW", "Wood", "Red")   //Корабль "motorShip1" построен
+var motorShip2 = motorYard.build("motorShip2", "200kW", "Metal", "Blue")  //Корабль "motorShip2" построен
+var sailingShip1 = sailingYard.build("sailingShip1", 3, 100, "Gray")      //Корабль "sailingShip1" построен
 
-motorYard.paint(sailingShip1)     //Корабль "sailingShip1" покрашен
-sailingYard.paint(motorShip1)     //Корабль "motorShip1" покрашен
+motorYard.paint(sailingShip1, "Green")     //Корабль "sailingShip1" в "Green" цвет
+sailingYard.paint(motorShip1, "Black")     //Корабль "motorShip1" "Black" цвет
 
-motorYard.repair(motorShip1)      //Корабль "motorShip1" отремонтирован
-sailingYard.repair(sailingShip1)  //Корабль "sailingShip1" отремонтирован
+motorYard.repair(motorShip1)      //Корабль "motorShip1" отремонтирован "05.07.2022"
+sailingYard.repair(sailingShip1)  //Корабль "sailingShip1" отремонтирован "05.07.2022"
 
 motorYard.repair(sailingShip1)    //Верфь "MotorYard" не подходит для корабля "sailingShip1"
 sailingYard.repair(motorShip1)    //Верфь "SailingYard" не подходит для корабля "motorShip1"
