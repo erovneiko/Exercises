6adedd6f1fdb2f672706942d2f7c70e2fb78817b
Evgeni Rovneiko <erovneiko@mail.ru>
2022-07-07 09:58:36 +0300

    updates

--- lesson_05/chess.js
+++ lesson_05/chess.js
@@ -29,20 +29,32 @@ class Queen extends Figure {   //Ферзь
 
 class Field {
   constructor () {
-    this.board = new Array(8)
-    this.board.fill(new Array(8))
+    this.board = {
+      'a': new Array(8),
+      'b': new Array(8),
+      'c': new Array(8),
+      'd': new Array(8),
+      'e': new Array(8),
+      'f': new Array(8),
+      'g': new Array(8),
+      'h': new Array(8)
+    }
     this.figures = []
   }
 }
 
-const cNewGame = 0
-const cInProgress = 1
-const cWhiteWon = 2
-const cBlackWon = 3
+const cStatusNewGame = 0
+const cStatusInProgress = 1
+const cStatusWhiteWon = 2
+const cStatusBlackWon = 3
+
+const cMoveWhite = 1
+const cMoveBlack = 2
 
 class Chess {
   constructor () {
-    this.status = cNewGame
+    this.status = cStatusNewGame
+    this.whoseMove = cMoveWhite
     this.killedFigures = []
     this.movesHistory = []
   }
