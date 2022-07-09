532a15c391546deb8803767d45616111f031d999
Evgeni Rovneiko <erovneiko@mail.ru>
2022-07-09 15:49:49 +0300

    updates

--- lesson_06/1_nodes.js
+++ lesson_06/1_nodes.js
@@ -1,8 +1,18 @@
-function parseTemplate(node, obj) {
+function setTextByDataField(node, obj) {
   node.querySelectorAll("[data-field]").forEach(elem => {
     if (obj[elem.dataset.field])
       elem.textContent = obj[elem.dataset.field]
     else
-      console.log(`Object property "${elem.dataset.field}" not found`)
-  });
+      throw Error(`Object property "${elem.dataset.field}" not found`);
+  })
+}
+
+function onLoad() {
+  setTextByDataField(
+    document.getElementById("item1"),
+    {
+      title: "Hello world",
+      description: "The first program",
+    }
+  )
 }
--- lesson_06/2_arrays.js
+++ lesson_06/2_arrays.js
@@ -1,23 +1,42 @@
-function parseArray(arr, node = null) {
-  if (arr.length) {
-    var ul = document.createElement("ul")
-    if (!node)
-      document.querySelector("div").append(ul)
-    else
-      node.append(ul)
-  }
+function createAndAppendTagUL(node) {
+  var ul = document.createElement("ul")
+
+  if (!node)
+    document.querySelector("div").append(ul)
+  else
+    node.append(ul)
+
+  return ul
+}
+
+function createAndAppendTagLI(node, li, text) {
+  li = document.createElement("li")
+  li.textContent = text
+  node.append(li)
+  return li
+}
+
+function createTagLI_IfNecessary(li) {
+  if (li)
+    return li
+  else
+    return document.createElement("li")
+}
+
+function renderArrayAsList(arr, node = null) {
+  if (arr.length)
+    var ul = createAndAppendTagUL(node)
 
   var li = null
-  
+
   arr.forEach(elem => {
-    if (typeof elem == "string") {
-      li = document.createElement("li")
-      li.textContent = elem
-      ul.append(li)
-    }
+    if (typeof elem == "string")
+      li = createAndAppendTagLI(ul, li, elem)
     else {
-      if (!li) li = document.createElement("li")
-      parseArray(elem, li)
+      li = createTagLI_IfNecessary(li)
+      renderArrayAsList(elem, li)
     }
-  });
+  })
 }
+
+renderArrayAsList(["Item", ["Item2", ["Item3"]]])
