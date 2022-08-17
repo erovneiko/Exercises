1c8fefc7341f923d2f9e0c4b940a0d8a2952048f
Evgeni Rovneiko <erovneiko@mail.ru>
2022-08-17 15:26:44 +0300

    updates

--- lesson_13/sources/main.js
+++ lesson_13/sources/main.js
@@ -1,20 +1,20 @@
-import {
-  cStatusNewGame,
-  cStatusInProgress,
-  cStatusWhiteWon,
-  cStatusBlackWon, 
-  cMoveWhite,
-  cMoveBlack
-} from './const.js'
+// import {
+//   cStatusNewGame,
+//   cStatusInProgress,
+//   cStatusWhiteWon,
+//   cStatusBlackWon, 
+//   cMoveWhite,
+//   cMoveBlack
+// } from './const.js'
 
-import { Figure } from './figure.js'
-import { Pawn } from './pawn.js'
-import { Rook } from './rook.js'
-import { Bishop } from './bishop.js'
-import { Knight } from './knight.js'
-import { King } from './king.js'
-import { Queen } from './queen.js'
-import { Field } from './field.js'
-import { Chess } from './chess.js'
+// import { Figure } from './figure.js'
+// import { Pawn } from './pawn.js'
+// import { Rook } from './rook.js'
+// import { Bishop } from './bishop.js'
+// import { Knight } from './knight.js'
+// import { King } from './king.js'
+// import { Queen } from './queen.js'
+// import { Field } from './field.js'
+// import { Chess } from './chess.js'
 
-console.log('Chess initialized!')
+// console.log('Chess initialized!')
